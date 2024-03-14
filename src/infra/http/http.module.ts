c9fe09004d6eb561325e827653f12eca6f3de238
Victor Sousa <victorbrito001@gmail.com>
2024-03-14 05:50:27 -0300

    Infraestrutura e injecao de dependencias

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,11 +1,24 @@
 import { Module } from '@nestjs/common';
-import { NotificationsController } from './controllers/notifcations.controller';
+
 import { SendNotification } from '@application/use-cases/send-notification';
 import { DatabaseModule } from '@infra/database/database.module';
+import { NotificationsController } from './controllers/notification.controller';
+import { CancelNotification } from '@application/use-cases/cancel-notification';
+import { CountRecipientNotifications } from '@application/use-cases/count-recipient-notifications';
+import { GetRecipientNotifications } from '@application/use-cases/get-recipient-notifications';
+import { ReadNotification } from '@application/use-cases/read-notification';
+import { UnreadNotification } from '@application/use-cases/unread.notification';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [NotificationsController],
-  providers: [SendNotification],
+  providers: [
+    SendNotification,
+    CancelNotification,
+    CountRecipientNotifications,
+    GetRecipientNotifications,
+    ReadNotification,
+    UnreadNotification,
+  ],
 })
 export class HttpModule {}
